373ed50c33cf1439e2f51f4a1815290880c13acb
b <vpbalint@silentsignal.hu>
2023-04-10 16:19:55 +0200

    preparing client config

---
+++ docker/client/startup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+./macaroon create -a config_host/auth.key | tee godmode.key | ./macaroon dump
+cat godmode.key | base64 -w0 > config_host/godmode.b64
+mkdir config
+cd python
+./genkey.py client.tmp | base64 -d >>client.tmp
+base64 < client.tmp > ../config/client.key
+cd ..
+python gen_config.py config_host/ | tee klutshnik.cfg
+/bin/sh -i 
