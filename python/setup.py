23219aefd6ed1355eee83d38c79cc64dc11d7b79
stf <7o5rfu92t@ctrlc.hu>
2025-05-17 14:52:11 +0200

    [mod] bumped py client to v0.2.0

--- python/setup.py
+++ python/setup.py
@@ -14,7 +14,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name = 'klutshnik',
-      version = '0.1.0',
+      version = '0.2.0',
       description = 'Klutshnik CLI client',
       license = "GPLv3",
       author = 'Stefan Marsiske',
