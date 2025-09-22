87c08b96d874aebd0a7ac548633766ea8f9e319f
stf <7o5rfu92t@ctrlc.hu>
2025-09-22 02:37:36 +0200

    [mod] switch to zstd compression for rpi images

---
+++ images/klutshnik-sbox-rpi/input/stages/90/50-compress.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+colour_echo ">> Compress images"
+# copy final image
+mkdir -p ${OUTPUT_PATH}
+zstd --ultra -22 -c ${IMAGE_PATH}/sdcard.img >${OUTPUT_PATH}/${IMG_NAME}.img.zst
+zstd --ultra -22 -c ${IMAGE_PATH}/rootfs.ext4 >${OUTPUT_PATH}/${IMG_NAME}_update.img.gz
