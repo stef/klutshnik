3264b82d74f745eae42448c085a3b81245174f55
stf <7o5rfu92t@ctrlc.hu>
2025-10-22 22:40:04 +0200

    [enh] added framework for generating seccomp bpf rulesets

---
+++ test/easy-test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd servers
+rm -rf */data/[0-9a-f]*
+ORACLE_STRACE=1 ./start-servers.sh &
+SERVERS_PID=$!
+cd ..
+./test.sh
+klutshnik delete "keyid1" <kltsk
+rm -fr otherclient/keystore/[0-9a-f]* keystore/[0-9a-f]*
+./test.sh
+echo kill "$SERVERS_PID"
+kill "$SERVERS_PID"
